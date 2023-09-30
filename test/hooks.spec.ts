c8c3f58477c5795a0ff1af895b0776da52737987
Kumar Programming <kkyprogramming@gmail.com>
2023-09-30 15:00:50 +0100

    hooks session

---
+++ test/hooks.spec.ts
@@ -0,0 +1,42 @@
+import { expect } from "chai";
+import Calculator from "../src/calculator";
+
+describe.only("hooks test", () => {
+
+  let calc:Calculator;
+  before(() => {
+    console.log("before");
+    calc = new Calculator();
+  });
+  beforeEach(() => {
+    console.log("before each");
+  });
+
+  afterEach(() => {
+    console.log("after each");
+  });
+  after(() => {
+    console.log("after");
+  });
+
+  describe('add test suite', ()=>{
+    it("should return sum", () => {
+      // arrange
+      // act
+      const result = calc.add(2, 3);
+      // assert
+      expect(result).to.equal(5);
+    });
+  })
+
+  
+  describe('subsctact test suite', ()=>{
+    it("should return substract", () => {
+      // arrange
+      // act
+      const result = calc.subsctract(5, 3);
+      // assert
+      expect(result).to.equal(2);
+    });
+  })
+});
